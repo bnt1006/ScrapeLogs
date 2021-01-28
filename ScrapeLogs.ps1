caf5f1444b48d58877a4873cd6d8b9a080093fda
Brian Thacker <neillthacker101@gmail.com>
2021-01-27 22:18:08 -0600

    Update ScrapeLogs.ps1

--- ScrapeLogs.ps1
+++ ScrapeLogs.ps1
@@ -5,9 +5,9 @@
     Scrapes EVTX log files found using wevutil for amount of time x minutes. Output as text data.
     Default 60 minutes. Default output in $env:TEMP\ScrapedLogs.txt
 .EXAMPLE Check all logs for the term "Windows Defender" in the past hour
-    PS/> Scrape-logs "Windows Defender" 60
+    PS/> Scrape-Logs "Windows Defender" 60
 .EXAMPLE Check all logs for the term "Windows Defender" and output to c:\testdata.txt
-    PS/> Scrape-logs "Windows Defender" -OutFile "c:\testdata.txt"
+    PS/> Scrape-Logs "Windows Defender" -OutFile "c:\testdata.txt"
 .NOTES
     Time is reference by minute. 1 hour = 60, 1 day = 1440, 1 week = 10080, 1 month ~= 43800, 1 year ~= 525600
 .LINK
